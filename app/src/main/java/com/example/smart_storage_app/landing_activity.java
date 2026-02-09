d6dce7d12d3d73cf5645beb033e580ea17c17630
JCDGuardiana <jayceeguardiana853@gmail.com>
2026-02-09 22:10:56 +0800

    update

---
+++ app/src/main/java/com/example/smart_storage_app/landing_activity.java
@@ -0,0 +1,12 @@
+package com.example.smart_storage_app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class landing_activity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_activity);
+    }
+}
